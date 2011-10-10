209ce8664157e48f1ec8763493b7a5610aa279cb
Alejandro Machado <alemacgo@gmail.com>
2011-10-11 01:09:36 +0200

    Serializing objects with cPickle

--- svenska.py
+++ svenska.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
-dict = { \
-    "går forbi" : "go past (a place)", \
-    "får motion" : "exercise",        \
-}
+#! /usr/bin/env python
 
-def l(key, value):
+import cPickle as pickle
+
+dict = {}
+box = []
+
+def _save():
+    file = open("dict.p", "wb")
+    pickle.dump(dict, file)
+    file.close()
+
+def _load():
+    global dict
+    file = open("dict.p", "rb")
+    dict = pickle.load(file)
+    file.close()
+
+def add(key, value):
     dict[key] = value
+    _save()
     print key, '-->', value
 
-def p():
-    for key in dictionary:
-        print "%s\t%s\t%s" %(key, '-->', dictionary[key])
+def show():
+    _load()
+    for key in dict:
+        print "%s\t%s\t%s" %(key, '-->', dict[key])
 
+if __name__ == '__main__':
+    pass
